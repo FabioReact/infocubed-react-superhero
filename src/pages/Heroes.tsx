c83e9f5d9154c43645e23c72454a1a4b5780b0ef
FabioReact <fabio.react.dev@gmail.com>
2023-06-27 12:07:14 +0200

    feat: add Waiting component to Heroes Page

--- src/pages/Heroes.tsx
+++ src/pages/Heroes.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import Fetcher, { BASE_URL } from '../api/fetcher'
 import { Hero } from '../types/hero'
 import HeroCard from '../components/HeroCard'
+import Waiting from '../components/Waiting'
+import Spinner from '../components/Spinner/Spinner'
 
 const letters: string[] = []
 for (let i = 97; i <= 122; i++) {
@@ -10,19 +12,20 @@ for (let i = 97; i <= 122; i++) {
 
 const Heroes = () => {
   const [heroes, setHeroes] = useState<Hero[]>([])
-  const [counter, setCounter] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
   const [selectedLetter, setSelectedLetter] = useState('a')
 
   useEffect(() => {
     console.log('Rendu initial du composant Heroes')
     const controller = new AbortController()
+    setIsLoading(true)
     // Recuperer de l'API tout les heroes commençant par la lettre A
     Fetcher.get(`${BASE_URL}/heroes?name_like=^${selectedLetter}`, {
       method: 'GET',
       signal: controller.signal,
     })
       .then((data) => {
-        console.log(data)
+        setIsLoading(false)
         setHeroes(data)
       })
       .catch((error) => {
@@ -35,17 +38,6 @@ const Heroes = () => {
       controller.abort()
     }
   }, [selectedLetter])
-
-  useEffect(() => {
-    console.log('Rendu initial du composant Heroes - Observer counter', counter)
-    return () => {
-      console.log('Destruction du composant Heroes - Oberver counter', counter)
-    }
-  }, [counter])
-
-  const increment = () => {
-    setCounter((c) => c + 1)
-  }
 
   const onClickLetter = (letter: string) => {
     setSelectedLetter(letter)
@@ -65,12 +57,13 @@ const Heroes = () => {
           </li>
         ))}
       </ul>
-      <p>Counter Value: {counter}</p>
-      <button onClick={increment}>Increment</button>
-      {/* <pre>{JSON.stringify(heroes, null, 2)}</pre> */}
-      <div className='flex flex-wrap gap-4 justify-center'>
-        {heroes.map(hero => <HeroCard key={hero.id} hero={hero} /> )}
-      </div>
+      <Waiting isLoading={isLoading}>
+        <div className='flex flex-wrap gap-4 justify-center'>
+          {heroes.map((hero) => (
+            <HeroCard key={hero.id} hero={hero} />
+          ))}
+        </div>
+      </Waiting>
     </section>
   )
 }
