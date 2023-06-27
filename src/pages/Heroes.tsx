107956d408e515bcd5c5dd379fb0f3cb42c63a8c
FabioReact <fabio.react.dev@gmail.com>
2023-06-27 10:57:31 +0200

    feat: add HeroCard for each hero

--- src/pages/Heroes.tsx
+++ src/pages/Heroes.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react'
+import Fetcher, { BASE_URL } from '../api/fetcher'
+import { Hero } from '../types/hero'
+import HeroCard from '../components/HeroCard'
 
 const letters: string[] = []
 for (let i = 97; i <= 122; i++) {
@@ -6,23 +9,32 @@ for (let i = 97; i <= 122; i++) {
 }
 
 const Heroes = () => {
-	const [heroes, setHeroes] = useState([])
+  const [heroes, setHeroes] = useState<Hero[]>([])
   const [counter, setCounter] = useState(0)
   const [selectedLetter, setSelectedLetter] = useState('a')
 
   useEffect(() => {
     console.log('Rendu initial du composant Heroes')
+    const controller = new AbortController()
     // Recuperer de l'API tout les heroes commençant par la lettre A
-    fetch('http://localhost:4000/heroes')
-      .then((response) => response.json())
+    Fetcher.get(`${BASE_URL}/heroes?name_like=^${selectedLetter}`, {
+      method: 'GET',
+      signal: controller.signal,
+    })
       .then((data) => {
-				console.log(data)
-				setHeroes(data)
-			})
+        console.log(data)
+        setHeroes(data)
+      })
+      .catch((error) => {
+        console.log({
+          error, // error: error
+        })
+      })
     return () => {
       console.log('Destruction du composant Heroes')
+      controller.abort()
     }
-  }, [])
+  }, [selectedLetter])
 
   useEffect(() => {
     console.log('Rendu initial du composant Heroes - Observer counter', counter)
@@ -42,17 +54,23 @@ const Heroes = () => {
   return (
     <section>
       <h1>Heroes List</h1>
-      <ul>
+      <ul className='flex justify-center gap-2 uppercase font-semibold text-xl my-4'>
         {letters.map((letter) => (
-          <li key={letter} onClick={() => onClickLetter(letter)}>
+          <li
+            className={`cursor-pointer ${letter === selectedLetter && 'text-red-600'}`}
+            key={letter}
+            onClick={() => onClickLetter(letter)}
+          >
             {letter}
           </li>
         ))}
       </ul>
       <p>Counter Value: {counter}</p>
       <button onClick={increment}>Increment</button>
-      <p>Vous avez cliquer sur: {selectedLetter}</p>
-			<pre>{JSON.stringify(heroes, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(heroes, null, 2)}</pre> */}
+      <div className='flex flex-wrap gap-4 justify-center'>
+        {heroes.map(hero => <HeroCard key={hero.id} hero={hero} /> )}
+      </div>
     </section>
   )
 }
