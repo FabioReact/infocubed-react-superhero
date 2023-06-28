04fc929c015117d36c882c3c006ffea70664a56b
FabioReact <fabio.react.dev@gmail.com>
2023-06-28 17:08:50 +0200

    fix: add return type to getHeroes

--- src/api/heroes.ts
+++ src/api/heroes.ts
@@ -1,3 +1,4 @@
+import { Hero } from '../types/hero'
 import Fetcher, { BASE_URL } from './fetcher'
 
 type GetHeroesParams = {
@@ -6,7 +7,7 @@ type GetHeroesParams = {
   speed?: string
 }
 
-export const getHeroes = ({ name, intelligence, speed }: GetHeroesParams) => {
+export const getHeroes = ({ name, intelligence, speed }: GetHeroesParams): Promise<Hero[]> => {
   let query = `name_like=${name}`
   if (intelligence) query += `&powerstats.intelligence_gte=${intelligence}`
   if (speed) query += `&powerstats.speed_gte=${speed}`
