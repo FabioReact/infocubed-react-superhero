70eb9c3987b318c4f768d13219aca27e94594f60
FabioReact <fabio.react.dev@gmail.com>
2023-06-28 09:59:57 +0200

    feat: add login form and subnit handler without validation nor API call

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,48 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Login = () => {
-	return (
-		<section>
-			<h1>Login</h1>
-		</section>
-	)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+	const navigate = useNavigate()
+
+	const onSubmitHandler = (event: React.FormEvent) => {
+		event.preventDefault()
+		console.log(email, password)
+		// Appel à l'API pour se connecter
+		// Erreur -> Afficher les erreurs
+		// Succès -> Redirection
+		navigate('/')
+	}
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={onSubmitHandler}>
+        <fieldset>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </fieldset>
+        <button type='submit'>Login</button>
+      </form>
+    </section>
+  )
 }
 
 export default Login
